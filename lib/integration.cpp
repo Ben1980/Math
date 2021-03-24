36dbb34d48e8313b5cecb5a8251d43eef4e9c488
Benjamin Mahr <Ben1980@users.noreply.github.com>
2021-03-24 15:37:22 +0000

    simpson integral

--- lib/integration.cpp
+++ lib/integration.cpp
@@ -19,3 +19,22 @@ double NumLib::TrapezoidalIntegration(double x1, double x2, size_t N, const std:
     
     return 0;
 }
+
+double NumLib::SimpsonIntegration(double x1, double x2, size_t N, const std::function<double (double)> &f) {
+    if(f) {
+        if(N < 1 ) N = 1;
+        const double width = (x2-x1)/N;
+
+        double simpson_integral = 0;
+        for(size_t step = 0; step < N; step++) {
+            const double x_i = x1 + step*width;
+            const double x_i1 = x1 + (step+1)*width;
+
+            simpson_integral += (x_i1-x_i)/6.0*(f(x_i) + 4.0*f(0.5*(x_i+x_i1)) + f(x_i1));
+        }
+
+        return simpson_integral;
+    }
+    
+    return 0;
+}
--- lib/integration.h
+++ lib/integration.h
@@ -39,6 +39,41 @@ namespace NumLib {
    * @return Integral of function \f$f\f$ in range \f$[a-b]\f$
    */
   double TrapezoidalIntegration(double x1, double x2, size_t N, const std::function<double (double)> &f);
+
+  /**
+   * @brief A numerical integration method using the simpson approach
+   * 
+   * SimpsonIntegration is using the simpson approach to acomplish numerical integrations as described at [thoughts-on-coding.com](https://thoughts-on-coding.com/2019/04/17/numerical-methods-in-cpp-part-1-newton-cotes-integration/)
+   * and calculates the integral of function \f$f\f$ in range \f$[a-b]\f$.
+   * 
+   * \f[
+   * w=\frac{b-a}{N}
+   * \f]
+   * \f[
+   * x_i=a+i \cdot w
+   * \f]
+   * \f[
+   * x_{i+1}=a+\left( i+1 \right) \cdot w
+   * \f]
+   * \f[
+   * I=\textstyle \sum_{0}^{N}\frac{x_{i+1}-x_i}{6}\left( f(x_i)+4f\left( \frac{x_1+x_2}{2} \right)+f(x_{i+1}) \right)
+   * \f]
+   * 
+   * @image html simpson.png
+   * 
+   * @test Test SimpsonIntegration, valid case: Testing method with function \f$I=\int_0^{\pi/2} \frac{5}{e^\pi-2}\exp(2x)\cos(x)dx=1.0\f$
+   * @test Test SimpsonIntegration, x1=x2=0: Testing method with zero range
+   * @test Test SimpsonIntegration, N=0: Testing method with zero iteration steps
+   * @test Test SimpsonIntegration, f=nullptr: Testing method with error in function f
+   * 
+   * @param x1 begining of Integration range \f$x_1=a\f$
+   * @param x2 end of Integration range \f$x_2=b\f$
+   * @param N number of iteration steps
+   * @param f function \f$f\f$ to integrate
+   * 
+   * @return Integral of function \f$f\f$ in range \f$[a-b]\f$
+   */
+  double SimpsonIntegration(double x1, double x2, size_t N, const std::function<double (double)> &f);
 };
 
 #include "testUtils.h"
@@ -86,5 +121,47 @@ TEST_CASE ("Test TrapezoidalIntegration, f=nullptr") {
   CHECK(TestUtils::IsValid(result, 0.0, 1e-3));
 }
 
+TEST_CASE ("Test SimpsonIntegration, valid case") {
+  const double PI = 3.14159265358979323846;
+  const double E = 2.71828182845904523536;
+
+  auto f = [E, PI](double x) { 
+    return 5.0/(std::pow(E, PI) - 2.0) * exp(2.0*x) * cos(x); 
+  };
+
+  const double result = NumLib::SimpsonIntegration(0, 0.5*PI, 100, f);
+  CHECK(TestUtils::IsValid(result, 1.0, 1e-3));
+}
+
+TEST_CASE ("Test SimpsonIntegration, x1=x2=0") {
+  const double PI = 3.14159265358979323846;
+  const double E = 2.71828182845904523536;
+
+  auto f = [E, PI](double x) { 
+    return 5.0/(std::pow(E, PI) - 2.0) * exp(2.0*x) * cos(x); 
+  };
+
+  const double result = NumLib::SimpsonIntegration(0, 0, 100, f);
+  CHECK(TestUtils::IsValid(result, 0.0, 1e-3));
+}
+
+TEST_CASE ("Test SimpsonIntegration, N=0") {
+  const double PI = 3.14159265358979323846;
+  const double E = 2.71828182845904523536;
+
+  auto f = [E, PI](double x) { 
+    return 5.0/(std::pow(E, PI) - 2.0) * exp(2.0*x) * cos(x); 
+  };
+
+  const double result = NumLib::SimpsonIntegration(0, 0.5*PI, 0, f);
+  CHECK(TestUtils::IsValid(result, 0.905, 1e-3));
+}
+
+TEST_CASE ("Test SimpsonIntegration, f=nullptr") {
+  const double PI = 3.14159265358979323846;
+
+  const double result = NumLib::SimpsonIntegration(0, 0.5*PI, 0, nullptr);
+  CHECK(TestUtils::IsValid(result, 0.0, 1e-3));
+}
 
 #endif
